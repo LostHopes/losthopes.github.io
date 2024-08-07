af11d0da5c0e60ae5b50b5f0e6cb30581cd14adf
losthope <arsen.melnuchyk@gmail.com>
2024-08-08 00:37:27 +0300

    implemented hamburger menu on small screens

--- themes/blog-gruvbox-dark/assets/js/menu.js
+++ themes/blog-gruvbox-dark/assets/js/menu.js
@@ -1,17 +1,12 @@
-function showBurger(item) {
-    let wHeight = window.innerHeight;
-    let wWidth = window.innerWidth;
-    let navBar = document.getElementsByClassName("nav-menu");
-    let element = document.createElement("div");
-
-    for (let item = 0; item < 3; item++) {
-        element.className = `line${item}`;
-        navBar.appendChild(element);
-    }
-
-    item.classList.toggle("burger");
-}
-
-function animateBurger(item) {
-
-}
+document.addEventListener("DOMContentLoaded", function () {
+    var burger = document.querySelector(".burger");
+    burger.addEventListener("click", function () {
+        burger.classList.toggle("burger-state");
+        let menu = document.querySelector(".nav-menu");
+        if (menu.style.display === "block") {
+            menu.style.display = "none";
+        } else {
+            menu.style.display = "block";
+        }
+    });
+});
