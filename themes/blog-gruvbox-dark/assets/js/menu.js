032fe93332577895e0960e453c1e992911d3ae48
losthope <arsen.melnuchyk@gmail.com>
2024-08-09 15:22:41 +0300

    wrapped js functions for hamburger menu into functions

--- themes/blog-gruvbox-dark/assets/js/menu.js
+++ themes/blog-gruvbox-dark/assets/js/menu.js
@@ -2,17 +2,19 @@ document.addEventListener("DOMContentLoaded", function () {
     var burger = document.querySelector(".burger");
     var menu = document.querySelector(".nav-menu");
 
-    burger.addEventListener("click", function () {
+    function toggleBurgerMenu() {
         burger.classList.toggle("burger-state");
-
         menu.classList.toggle("nav-menu");
-    });
+    }
 
-    window.addEventListener("resize", function () {
+    function handleWindowResize() {
         if (window.innerWidth > 768) {
             menu.classList.remove("hidden-menu");
             menu.classList.add("nav-menu");
             burger.classList.remove("burger-state");
         }
-    });
+    }
+
+    burger.addEventListener("click", toggleBurgerMenu);
+    window.addEventListener("resize", handleWindowResize);
 });
