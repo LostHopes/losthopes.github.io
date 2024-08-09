2307afaaea1498dc09ef2f69ea15263be7ba49a1
losthope <arsen.melnuchyk@gmail.com>
2024-08-09 15:15:40 +0300

    fixed bug when resizing disappeared navigation menu

--- themes/blog-gruvbox-dark/assets/js/menu.js
+++ themes/blog-gruvbox-dark/assets/js/menu.js
@@ -1,12 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     var burger = document.querySelector(".burger");
+    var menu = document.querySelector(".nav-menu");
+
     burger.addEventListener("click", function () {
         burger.classList.toggle("burger-state");
-        let menu = document.querySelector(".nav-menu");
-        if (menu.style.display === "block") {
-            menu.style.display = "none";
-        } else {
-            menu.style.display = "block";
+
+        menu.classList.toggle("nav-menu");
+    });
+
+    window.addEventListener("resize", function () {
+        if (window.innerWidth > 768) {
+            menu.classList.remove("hidden-menu");
+            menu.classList.add("nav-menu");
+            burger.classList.remove("burger-state");
         }
     });
 });
