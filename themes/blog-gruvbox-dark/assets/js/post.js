0a1fcef182a719fd8095d2a60b0da9e37bebba52
losthope <arsen.melnuchyk@gmail.com>
2025-04-13 00:12:16 +0300

    fix: scroll to the top arrow appears in the initial location when reloading the page

--- themes/blog-gruvbox-dark/assets/js/post.js
+++ themes/blog-gruvbox-dark/assets/js/post.js
@@ -1,25 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     var scroll = document.querySelector(".scroll-btn");
-    
+
 
     function scrollToTop() {
         window.scrollTo({
             top: 0,
             behavior: "smooth"
         })
-    } 
-    
-    if(!scroll) { return; }
+    }
 
-    function toggleScrollButton () {        
+    if (!scroll) { return; }
+
+    function toggleScrollButton() {
         let scrollY = window.scrollY;
-        if (scrollY < 800){
+        if (scrollY > 800) {
             scroll.classList.add("scroll-btn-hide");
-        } else {
-            scroll.classList.remove("scroll-btn-hide");
+            return;
         }
+        scroll.classList.remove("scroll-btn-hide");
     }
 
     scroll.addEventListener("click", scrollToTop);
     document.addEventListener("scroll", toggleScrollButton);
+    window.addEventListener("load", toggleScrollButton);
 });
