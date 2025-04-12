7495f0a3df8bddc5f07e54422280d06246b931e6
losthope <arsen.melnuchyk@gmail.com>
2025-04-12 15:38:36 +0300

    feat: hide the scroll-btn at certain position

--- themes/blog-gruvbox-dark/assets/js/post.js
+++ themes/blog-gruvbox-dark/assets/js/post.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     var scroll = document.querySelector(".scroll-btn");
+    
 
     function scrollToTop() {
         window.scrollTo({
@@ -10,5 +11,15 @@ document.addEventListener("DOMContentLoaded", function() {
     
     if(!scroll) { return; }
 
+    function toggleScrollButton () {        
+        let scrollY = window.scrollY;
+        if (scrollY < 800){
+            scroll.classList.add("scroll-btn-hide");
+        } else {
+            scroll.classList.remove("scroll-btn-hide");
+        }
+    }
+
     scroll.addEventListener("click", scrollToTop);
+    document.addEventListener("scroll", toggleScrollButton);
 });
